633920f631bd6fdb7c13df3895e1638b22753eb8
cooperjp <jpryancooper@gmail.com>
2025-02-04 16:21:15 -0700

    js

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://cooperjp.github.io',  // Replace with your GitHub username
-  base: '/cooperjp.github.io',  // Replace with your repository name
+  base: '/',  // Replace with your repository name
   outDir: './dist',
   build: {
     assets: 'assets'
